d05a0802070c2ac1ed4489ae547659064fe06022
Jerry <sweetandpain@163.com>
2019-12-30 18:07:48 +0800

    工厂方法模式实战

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -5,6 +5,9 @@ public class Cat {
     private String name;
     private int age;
 
+    public Cat(String name, int age) {
+    }
+
     /**
      * 创建一只猫的工厂方法。当传入的参数无效，即：
      *
@@ -18,7 +21,13 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if (age < 0 || name == null || name == "") {
+            return INVALID_CAT;
+        } else {
+            return new Cat(name, age);
+        }
+    }
 
     public String getName() {
         return name;
