1e5576a26e43446733648f3e49eb9bd529960893
Truthcode <617805327@qq.com>
2020-03-04 17:06:09 +0800

    工厂

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -5,7 +5,7 @@ public class Cat {
     private String name;
     private int age;
 
-    public Cat(String name, int age) {
+     Cat(String name, int age) {
         this.name = name;
         this.age = age;
     }
@@ -26,10 +26,11 @@ public class Cat {
 
 
     public static Cat newCat(String name, int age) {
-        if (age < 0 || name.isEmpty() || name == null){
+        if (name == null || name.isEmpty() || age < 0 ){
             return INVALID_CAT;
-        }
+        }else {
             return new Cat(name,age);
+        }
     }
 
     public String getName() {
