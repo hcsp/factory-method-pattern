4521f3198e5a848624c4f5f8c2b9b63e82cf3437
Administrator <756433556@qq.com>
2020-05-17 01:16:24 +0800

    工厂方法1111

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -4,10 +4,7 @@ public class Cat {
     private static final Cat INVALID_CAT = new Cat("Invalid cat", -1);
     private String name;
     private int age;
-    private Cat(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+
 
 
     /**
@@ -23,14 +20,18 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {
-    if (age < 0 || "".equals(name) || name == null) {
-        return INVALID_CAT;
-    } else {
-        return newCat(name, age);
+    private Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
+    public static Cat newCat(String name, int age) {
+        if ((name == null || name.length() < 1) || age < 0) {
+            return INVALID_CAT;
         }
+
+        return new Cat(name, age);
+    }
 
 
     public String getName() {
