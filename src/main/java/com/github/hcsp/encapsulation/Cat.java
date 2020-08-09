db2850bad6ae96592fc6889eba478e8f47a3e51e
pengyj <961942635@qq.com>
2020-08-09 18:20:28 +0800

    cat工厂模式

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -14,15 +14,15 @@ public class Cat {
      *
      * <p>否则，返回一只新创建的猫
      *
-     * @param age 年龄
+     * @param age  年龄
      * @param name 名字
      * @return 创建的猫
      */
     public static Cat newCat(String name, int age) {
-        if ( age<0 || name.isEmpty() || name == null) {
+        if (age < 0 || name.isEmpty() || name == null) {
             return INVALID_CAT;
         } else {
-            return newCat(name,age);
+            return newCat(name, age);
         }
     }
 
